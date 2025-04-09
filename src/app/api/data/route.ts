cb6cbc0672501bf7dff25be8d6d6ec22f5843785
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-09 09:05:18 +0200

    [ADD]: Data fetching route and fetching logic

---
+++ src/app/api/data/route.ts
@@ -0,0 +1,39 @@
+import { auth, prisma } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET (req: NextRequest) {
+    try {
+        // Get the user's session data
+        const session = await auth.api.getSession({
+            headers: await headers()
+        })
+
+        const id = session?.user.id;
+
+        // Get the user's data
+        const data = await prisma.user.findUnique({
+            where: {
+                id
+            },
+            include: {
+                appointments: true,
+                availability: true,
+                notifications: true,
+                settings: true,
+                services: true,
+                clients: true
+            }
+        })
+
+        return NextResponse.json(data)
+    } catch (e) {
+        console.log("An error occured while getting user data: ", e);
+
+        return NextResponse.json({
+            success: false,
+            message: "An error occured while getting user data",
+        }, { status: 500 });
+    }
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function Dashboard() {
-  return <section></section>;
+  // Get the user's data
+  const {data, isLoading: loading} = useQuery({
+    queryKey: ['data'],
+    queryFn: async () => {
+      const res = await axios.get("/api/data");
+
+      return res.data;
+    }
+  })
+
+  console.log("User data: ", data);
+
+  return (
+    <section>
+
+  </section>
+  )
 }
 
 export default Dashboard;
