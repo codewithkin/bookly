b6b1782d5f926cd87bb190442fb525ffaeafcf4d
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-09 06:29:44 +0200

    [ADD]: Shadcn Toaster to root layout file (to make the toast() function work globally)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import {Inter} from "next/font/google";
+import { Toaster } from "sonner";
 
 const inter = Inter({
   subsets: ['latin'],
@@ -38,6 +39,7 @@ export default function RootLayout({
         className={`${inter.className} antialiased`}
       >
         {children}
+        <Toaster richColors expand  />
       </body>
     </html>
   );
