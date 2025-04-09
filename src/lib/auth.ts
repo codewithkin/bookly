f6da9e6cdbf10d58a3489ce25176d7a5049a2290
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-09 06:24:14 +0200

    [ADD]: Prisma initialization [ADD]: Better-auth DB tables

---
+++ src/lib/auth.ts
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth";
+import { magicLink } from "better-auth/plugins";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
+import { PrismaClient } from "@/generated/prisma";
+ 
+export const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql"
+    }),
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+        }, 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID as string, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    },
+    plugins: [
+        magicLink   ({
+            sendMagicLink: async ({ email, token, url }, request) => {
+                // send email to user
+            }
+        }),
+        nextCookies() // Psst, Kin...Make sure this is the last plugin
+    ]
+});
