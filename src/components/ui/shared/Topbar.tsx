2211f2081360c19ba751c857a383d88c6f04f0f1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-09 08:27:16 +0200

    Code format

--- src/components/ui/shared/Topbar.tsx
+++ src/components/ui/shared/Topbar.tsx
@@ -19,7 +19,9 @@ function Topbar() {
 
   return (
     <article className="w-full flex justify-between md:px-12 items-center bg-white p-4 border-b border-blue-200">
-      <h2 className="text-2xl font-semibold text-primary hidden md:block">Bookly</h2>
+      <h2 className="text-2xl font-semibold text-primary hidden md:block">
+        Bookly
+      </h2>
 
       {/* User avatar and name */}
       <div className="flex items-center gap-2">
@@ -45,8 +47,12 @@ function Topbar() {
           </article>
         ) : (
           <article className="flex flex-col items-cente">
-            <h3 className="text-semibold text-xl">{user?.name || user?.email}</h3>
-            {user?.email && <p className="text-slate-500 text-md">{user?.email}</p>}
+            <h3 className="text-semibold text-xl">
+              {user?.name || user?.email}
+            </h3>
+            {user?.email && (
+              <p className="text-slate-500 text-md">{user?.email}</p>
+            )}
           </article>
         )}
       </div>
