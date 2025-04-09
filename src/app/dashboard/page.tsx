ae9dcde9df3b8c7aadc9248b769aebf3b9002ee9
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-04-09 09:06:26 +0200

    Code format

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,22 +5,18 @@ import axios from "axios";
 
 function Dashboard() {
   // Get the user's data
-  const {data, isLoading: loading} = useQuery({
-    queryKey: ['data'],
+  const { data, isLoading: loading } = useQuery({
+    queryKey: ["data"],
     queryFn: async () => {
       const res = await axios.get("/api/data");
 
       return res.data;
-    }
-  })
+    },
+  });
 
   console.log("User data: ", data);
 
-  return (
-    <section>
-
-  </section>
-  )
+  return <section></section>;
 }
 
 export default Dashboard;
